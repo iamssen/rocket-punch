b18a30bd5c87ce4b715169603f5ae04f9cec1ef2
SSen <i@ssen.name>
2020-06-13 16:56:52 +0900

    fix: remove useless clean

--- src/build-packages.ts
+++ src/build-packages.ts
@@ -1,20 +1,19 @@
-import fs from 'fs-extra';
 import path from 'path';
 import { build } from 'rocket-punch';
 import { buildMessageHandler } from 'rocket-punch/message-handlers/build';
 
-[
-  path.join(process.cwd(), 'node_modules/@ssen'),
-  path.join(process.cwd(), 'node_modules/rocket-punch'),
-].forEach((p) => {
-  if (fs.existsSync(p)) {
-    if (fs.statSync(p).isDirectory()) {
-      fs.rmdirSync(p, { recursive: true });
-    } else {
-      fs.unlinkSync(p);
-    }
-  }
-});
+//[
+//  path.join(process.cwd(), 'node_modules/@ssen'),
+//  path.join(process.cwd(), 'node_modules/rocket-punch'),
+//].forEach((p) => {
+//  if (fs.existsSync(p)) {
+//    if (fs.statSync(p).isDirectory()) {
+//      fs.rmdirSync(p, { recursive: true });
+//    } else {
+//      fs.unlinkSync(p);
+//    }
+//  }
+//});
 
 build({
   cwd: process.cwd(),
