816db3685e6b4338321c4d6b935f18909c6ede99
SSen <i@ssen.name>
2020-07-09 12:07:53 +0900

    test: e2e test for windows

---
+++ test/e2e.ps1
@@ -0,0 +1,8 @@
+# VARIABLES
+# ==================================================----------------------------------
+$ROOT=$pwd.Path;
+$VERDACCIO_PORT=4874;
+$LOCAL_REGISTRY_URL="http://localhost:$VERDACCIO_PORT/";
+
+echo "ROOT=$ROOT";
+echo "LOCAL_REGISTRY_URL=$LOCAL_REGISTRY_URL";
