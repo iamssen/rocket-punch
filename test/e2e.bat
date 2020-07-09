c553ed532bf7085043e9a3076e2837a31f327ced
SSen <i@ssen.name>
2020-07-09 15:06:14 +0900

    test: change windows e2e test from .ps1 to .bat

---
+++ test/e2e.bat
@@ -0,0 +1,8 @@
+REM VARIABLES
+REM ==================================================----------------------------------
+SET ROOT="%cd%"
+SET /A VERDACCIO_PORT=4874
+SET LOCAL_REGISTRY_URL="http://localhost:%VERDACCIO_PORT%/";
+
+echo "ROOT=%ROOT%"
+echo "LOCAL_REGISTRY_URL=%LOCAL_REGISTRY_URL%";
