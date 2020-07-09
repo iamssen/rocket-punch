dc3be53976c62ac282bf80591053e32786300a3c
SSen <i@ssen.name>
2020-07-09 15:29:53 +0900

    test: try e2e on windows

--- test/e2e.bat
+++ test/e2e.bat
@@ -1,8 +1,26 @@
-REM VARIABLES
-REM ==================================================----------------------------------
-SET ROOT="%cd%"
-SET /A VERDACCIO_PORT=4874
-SET LOCAL_REGISTRY_URL="http://localhost:%VERDACCIO_PORT%/";
+rem VARIABLES
+rem ==================================================----------------------------------
+set ROOT="%cd%"
+set /A VERDACCIO_PORT=4874
+set LOCAL_REGISTRY_URL="http://localhost:%VERDACCIO_PORT%/"
 
 echo "ROOT=%ROOT%"
-echo "LOCAL_REGISTRY_URL=%LOCAL_REGISTRY_URL%";
+echo "LOCAL_REGISTRY_URL=%LOCAL_REGISTRY_URL%"
+
+rem SETUP LOCAL REGISTRY
+rem ==================================================----------------------------------
+
+rem TODO functions... 
+rem TODO handleError, handleExit, trap
+
+if exist "%ROOT%/test/storage" (
+  tree "%ROOT%/test/storage"
+  rmdir /S /Q "%ROOT%/test/storage"
+)
+
+start /B "npx verdaccio@latest --config %ROOT%/test/verdaccio.yaml --listen %VERDACCIO_PORT%"
+timeout 30 > nul
+
+rem LOCAL PUBLISH
+rem ==================================================----------------------------------
+npx ts-node -r tsconfig-paths/register src/publish-packages-e2e.ts --tag e2e --registry "%LOCAL_REGISTRY_URL%";
