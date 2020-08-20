bcd824abb74a6c53e89f8f3ed8fb3bcd67c0833b
SSen <i@ssen.name>
2020-08-20 18:28:49 +0900

    chore: add build argv --out-dir

--- src/build-packages.ts
+++ src/build-packages.ts
@@ -2,23 +2,14 @@ import path from 'path';
 import { build } from 'rocket-punch';
 import { readEntry } from 'rocket-punch/entry/readEntry';
 import { buildMessageHandler } from 'rocket-punch/message-handlers/build';
+import yargs from 'yargs';
 
-//[
-//  path.join(process.cwd(), 'node_modules/@ssen'),
-//  path.join(process.cwd(), 'node_modules/rocket-punch'),
-//].forEach((p) => {
-//  if (fs.existsSync(p)) {
-//    if (fs.statSync(p).isDirectory()) {
-//      fs.rmdirSync(p, { recursive: true });
-//    } else {
-//      fs.unlinkSync(p);
-//    }
-//  }
-//});
+const argv = yargs.argv;
+const outDir: string | undefined = typeof argv['out-dir'] === 'string' ? argv['out-dir'] : undefined;
 
 build({
   cwd: process.cwd(),
   entry: readEntry({ cwd: process.cwd() }),
-  dist: path.join(process.cwd(), 'out/packages'),
+  dist: outDir ?? path.join(process.cwd(), 'out/packages'),
   onMessage: buildMessageHandler,
 });
