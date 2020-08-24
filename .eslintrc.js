a7a854bde0dfcd7e9d2b02fe88a45246790c59ee
SSen <i@ssen.name>
2020-08-24 17:18:11 +0900

    chore: cleanup files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,3 @@
 module.exports = {
   extends: ['@ssen', 'prettier', 'prettier/react', 'prettier/@typescript-eslint'],
-  //overrides: [
-  //  {
-  //    files: ['**/*.ts?(x)'],
-  //    rules: {
-  //      '@typescript-eslint/typedef': [
-  //        'error',
-  //        {
-  //          arrayDestructuring: false,
-  //          arrowParameter: false,
-  //          objectDestructuring: false,
-  //          parameter: true,
-  //          propertyDeclaration: true,
-  //          memberVariableDeclaration: true,
-  //          variableDeclaration: true,
-  //        },
-  //      ],
-  //    },
-  //  },
-  //],
 };
