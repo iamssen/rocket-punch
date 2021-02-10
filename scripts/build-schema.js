49858324d678ea456c6085d1ce51b32cfef4f0fa
SSen <i@ssen.name>
2021-02-10 12:38:13 +0900

    feat: support schema.json for .packages.yaml

---
+++ scripts/build-schema.js
@@ -0,0 +1,87 @@
+const fs = require('fs-extra');
+const path = require('path');
+const fetch = require('node-fetch');
+
+const schema = {
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  id:
+    'https://raw.githubusercontent.com/rocket-hangar/rocket-punch/master/schema.json#',
+  type: 'object',
+  properties: {
+    version: {
+      type: 'string',
+      description:
+        'Version must be parseable by node-semver (e.g. 0.1.0, 1.0.0-alpha.1...)',
+    },
+    tag: {
+      type: 'string',
+      default: 'latest',
+      description:
+        'tag (for example, `next` of `npm install react@next` when you install a package)',
+    },
+    access: {
+      type: 'string',
+      enum: ['public', 'private'],
+      description:
+        'if you set this value, it will pass to the `npm publish` command like this `npm publish --access public`',
+    },
+    registry: {
+      type: 'string',
+      description:
+        'if you set this value, it will pass to the `npm publish` command like this `npm publish --registry http://...`.',
+    },
+    module: {
+      type: 'string',
+      enum: ['commonjs', 'esm'],
+      description: 'module type',
+    },
+    compilerOptions: {
+      $ref: '#/definitions/compilerOptions',
+    },
+    packageJson: {
+      $ref: 'https://json.schemastore.org/package',
+    },
+  },
+  required: ['version'],
+};
+
+(async function () {
+  const {
+    definitions: {
+      compilerOptionsDefinition: {
+        properties: {
+          compilerOptions: {
+            properties: {
+              module,
+              moduleResolution,
+              skipLibCheck,
+              sourceMap,
+              declaration,
+              ...properties
+            },
+            ...compilerOptions
+          },
+        },
+      },
+    },
+  } = await fetch('https://json.schemastore.org/tsconfig.json').then((res) =>
+    res.json(),
+  );
+
+  fs.writeFileSync(
+    path.resolve(__dirname, '../schema.json'),
+    JSON.stringify(
+      {
+        ...schema,
+        definitions: {
+          compilerOptions: {
+            ...compilerOptions,
+            properties,
+          },
+        },
+      },
+      null,
+      2,
+    ),
+  );
+})();
